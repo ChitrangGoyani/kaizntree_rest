8b0b4e83e16237bf8ed34797d2f2db4268aae612
ChitrangGoyani <chitranggoyani@gmail.com>
2024-02-10 11:37:54 -0500

    created user auth endpoints

--- kaizntree_rest/urls.py
+++ kaizntree_rest/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('', include('dashboard_api.urls')),
+    path('user/', include('auth_api.urls'))
 ]
