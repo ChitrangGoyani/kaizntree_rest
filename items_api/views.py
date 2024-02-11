0bca60ab08abefeea2db3c47e6e4ba3ee75895ce
ChitrangGoyani <chitranggoyani@gmail.com>
2024-02-10 21:27:11 -0500

    added filtering and pagination

--- items_api/views.py
+++ items_api/views.py
@@ -1,22 +1,27 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from base.models import Item
+from items_api.filters import ItemFilter
 from .serializers import ItemSerializer
-from rest_framework import status
-from rest_framework.decorators import api_view, authentication_classes, permission_classes
-from rest_framework.authentication import SessionAuthentication, TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import status, filters
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from django_filters.utils import translate_validation
 
 from django.shortcuts import get_object_or_404
-from rest_framework.authtoken.models import Token
 
 @api_view(['GET'])
 def getItems(request):
-    items = Item.objects.all()
-    serializer = ItemSerializer(items, many=True)
-    return Response(serializer.data)
+    paginator = PageNumberPagination()
+    paginator.page_size = 2
+    filterset = ItemFilter(request.GET, queryset=Item.objects.all())
+    if not filterset.is_valid():
+        raise translate_validation(filterset.errors)
+    queryset = paginator.paginate_queryset(filterset.qs, request)
+    serializer = ItemSerializer(queryset, many=True)
+    return paginator.get_paginated_response(serializer.data)
 
 @api_view(['POST'])
 def addItem(request):
@@ -39,13 +44,11 @@ def getItem(request):
 
 @api_view(['GET'])
 def filterItems(request):
-    standard_filters = ['category','tag']
-    request_filters = request.GET
-    print(request_filters)
-    
-    # queryset = Item.objects.filter(**filter_query)
-    # serializer = ItemSerializer(queryset, many=True)
-    # return Response(serializer.data)
-    return Response(status=status.HTTP_200_OK)
+    items_qs = Item.objects.all()
+    filterset = ItemFilter(request.GET, queryset=items_qs)
+    if filterset.is_valid():
+        items_qs = filterset.qs
+    serializer = ItemSerializer(items_qs, many=True)
+    return Response(serializer.data)
 
 
